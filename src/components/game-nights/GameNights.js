4b6f9bbe9b5f11233cf49761e3df7898d07a537f
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-09-14 14:00:53 -0500

    refactored code to re-user GameNights component under profile

--- src/components/game-nights/GameNights.js
+++ src/components/game-nights/GameNights.js
@@ -13,17 +13,28 @@ class GameNights extends Component {
   activeUser = parseInt(sessionStorage.getItem("activeUser"))
 
   componentDidMount() {
-    console.log({...this.props})
     this.getAllGameNights()
   }
 
   getAllGameNights = () => {
-    GameNightManager.getAllGameNights()
-      .then(gameNights => {
-        this.setState({
-          gameNights: gameNights,
+    this.props.match.path.includes("profile")
+      ? this.props.getAllUserGameNights()
+      : GameNightManager.getAllGameNights()
+        .then(gameNights => {
+          this.setState({
+            gameNights: gameNights,
+          })
         })
-      })
+  }
+
+  determineGameNightsArr = () => {
+    let gameNightsArr = []
+    if (this.state.gameNights.length > 0) {
+      gameNightsArr = this.state.gameNights
+    } else {
+      gameNightsArr = this.props.gameNights
+    }
+    return gameNightsArr
   }
 
   handleDeleteGameNightOnClick = (gameNightId) => {
@@ -34,15 +45,16 @@ class GameNights extends Component {
   render() {
     return (
       <div className="gameNights__div">
-        {this.state.gameNights.map(gameNight =>
-          <GameNightCard
-            key={gameNight.id}
-            gameNight={gameNight}
-            friendData={this.props.friendData}
-            getAllFriendData={this.props.getAllFriendData}
-            handleDeleteGameNightOnClick={this.handleDeleteGameNightOnClick}
-          />
-        )}
+        {this.determineGameNightsArr()
+          ? this.determineGameNightsArr().map(gameNight =>
+            <GameNightCard
+              key={gameNight.id}
+              gameNight={gameNight}
+              friendData={this.props.friendData}
+              getAllFriendData={this.props.getAllFriendData}
+              handleDeleteGameNightOnClick={this.handleDeleteGameNightOnClick}/>)
+          : null
+        }
       </div>
     )
   }
